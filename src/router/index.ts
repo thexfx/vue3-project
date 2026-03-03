33fdc0caf96b6be3615d7b49f40240bfe9896250
thexfx <thexfx1@gmail.com>
2026-03-03 20:57:16 +0800

    feat: add new routes for account, transactions, transfer, deposit, withdraw

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,60 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import BankHomeView from '../views/BankHomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login',
+      component: LoginView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/home',
+      name: 'home',
+      component: BankHomeView,
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: () => import('../views/AccountDetailView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/transactions',
+      name: 'transactions',
+      component: () => import('../views/TransactionDetailView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/transfer',
+      name: 'transfer',
+      component: () => import('../views/TransferView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/deposit',
+      name: 'deposit',
+      component: () => import('../views/DepositView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/withdraw',
+      name: 'withdraw',
+      component: () => import('../views/WithdrawView.vue'),
+      meta: { requiresAuth: true },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const user = localStorage.getItem('bank_user')
+  if (to.meta.requiresAuth && !user) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
